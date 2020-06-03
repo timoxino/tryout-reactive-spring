bf58579e38acafe34b1f6d25db9351e4ef4d14c2
Tsimafei Shchytkavets <tsimafei.shchytkavets@equifax.com>
2020-06-03 15:01:48 -0400

    add empty webflux project

---
+++ src/test/java/com/timoxino/tryout/reactive/spring/ReactiveSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timoxino.tryout.reactive.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveSpringApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
